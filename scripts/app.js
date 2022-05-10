cf736d9f33a0c1ab6da0156f9a8964d290dcd6b7
Jae-Yu Yeh <jaeyu.yeh@gmail.com>
2022-05-10 16:42:42 -0700

    Add tab function for work section

--- scripts/app.js
+++ scripts/app.js
@@ -27,3 +27,45 @@ function scrollFunction() {
         navbar.classList.remove('scrolled')
     }
 }
+
+
+
+(function ($) {
+    // Variables
+    const $tabLink = $('#tabs-section .tab-link');
+    const $tabBody = $('#tabs-section .tab-body');
+    let timerOpacity;
+
+    // Toggle Class
+    const init = () => {
+        // Menu Click
+        $tabLink.off('click').on('click', function (e) {
+            // Prevent Default
+            e.preventDefault();
+            e.stopPropagation();
+
+            // Clear Timers
+            window.clearTimeout(timerOpacity);
+
+            // Toggle Class Logic
+            // Remove Active Classes
+            $tabLink.removeClass('active ');
+            $tabBody.removeClass('active ');
+            $tabBody.removeClass('active-content');
+
+            // Add Active Classes
+            $(this).addClass('active');
+            $($(this).attr('href')).addClass('active');
+
+            // Opacity Transition Class
+            timerOpacity = setTimeout(() => {
+                $($(this).attr('href')).addClass('active-content');
+            }, 50);
+        });
+    };
+
+    // Document Ready
+    $(function () {
+        init();
+    });
+}(jQuery));
